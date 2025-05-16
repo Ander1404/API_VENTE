bae5040997ebf85917ebc80fbd2c3307a077a3bd
ander1404 <jeanpaulkilingo024@gmail.com>
2025-05-16 15:11:40 +0100

    Refactor code structure for improved readability and maintainability

--- DETAILAPPROVISIONNEMENT/Get.php
+++ DETAILAPPROVISIONNEMENT/Get.php
@@ -1,11 +1,29 @@
 <?php
+// include('../CONNEXION/Db.php');		
+// $db=new Database();
+// $id=$_POST['id'];
+// $result=array();
+//     $conn = $db->connect();
+//     $stmt = $conn->query("SELECT * FROM  `vdetailapprovisionnement`where id_approvisionnement=$id ");
+//     // $stmt->bindParam(':id', $id);
+//     $result= $stmt->fetchAll(PDO::FETCH_ASSOC);
+// echo json_encode($result);
+
 include('../CONNEXION/Db.php');		
-$db=new Database();
-$id=$_POST['id'];
-$result=array();
+$db = new Database();
+$result = [];
+
+if (isset($_POST['id']) && is_numeric($_POST['id'])) {
+    $id = (int) $_POST['id']; // on force un entier pour plus de sécurité
     $conn = $db->connect();
-    $stmt = $conn->query("SELECT * FROM  `vdetailapprovisionnement`where id_approvisionnement=$id ");
-    // $stmt->bindParam(':id', $id);
-    $result= $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    $stmt = $conn->prepare("SELECT * FROM `vdetailapprovisionnement` WHERE id_approvisionnement = :id");
+    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+    $stmt->execute();
+
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+} else {
+    $result = ['error' => 'ID invalide ou manquant'];
+}
+
 echo json_encode($result);
-?>
